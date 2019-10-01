761c70078c7b14b079d216393f0b729562af0cf8
Vladimir Golub <golubvlauri@gmail.com>
2019-10-01 20:49:26 +0200

    fix App js file

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
 import React from 'react';
-import styled,{ createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import * as actionTypes from './actions/actions';
 
-import Container from './grid/Container';
-import Row from './grid/Row';
-import Column from './grid/Column';
-import ListElement from './components/ListElement';
-
-const ListSection = styled.div`
-  color: black;
-  font-size: 14px;
-  font-family: sans-serif, serif;
-  padding: 10px 0;
-`;
+import Layout from './components/common/Layout';
+import List from './conteiners/List';
+import FormAdd from './conteiners/FormAdd';
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-  
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: 'GraphikLCG-Regular', sans-serif;
-  }
+    * {
+        box-sizing: border-box;
+    }
+    
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'GraphikLCG-Regular', sans-serif;
+        color: black;
+        font-size: 14px;
+    }
 `;
 
 function App() {
-  return (
+    return (
     <>
-      <ListSection>
-        <Container>
-          <Row>
-            <Column
-              lg={12}
-              md={12}
-              sm={12}
-              xs={12}
-            >
-              <ListElement/>
-            </Column>
-          </Row>
-        </Container>
-      </ListSection>
-        { this.props.items }
-      <GlobalStyles/>
+        <Layout>
+            <Router>
+                <Switch>
+                    <Route path={'/'} exact={true} component={List} />
+                    <Route path={'/form'} component={FormAdd} />
+                </Switch>
+            </Router>
+        </Layout>
+        <GlobalStyles/>
     </>
   );
 }
@@ -55,11 +45,11 @@ const mapStateToProps = state => {
     }
 };
 
-const mapDispatch = dispatch => {
+const mapDispatchToProps = dispatch => {
     return {
         onAddItem: (value) => dispatch({ type: actionTypes.ADD_ITEM, value}),
         onDeleteItem: (value) => dispatch({ type: actionTypes.DELETE_ITEM, value })
     }
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
