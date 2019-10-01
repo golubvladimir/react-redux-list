43e2525bd1118c22e7d8effc8535028e64c9d98b
Vladimir Golub <golubvlauri@gmail.com>
2019-10-01 23:49:24 +0200

    fix App js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import * as actionTypes from './actions/actions';
 
 import Layout from './components/common/Layout';
 import List from './conteiners/List';
@@ -25,31 +22,18 @@ const GlobalStyles = createGlobalStyle`
 
 function App() {
     return (
-    <>
-        <Layout>
-            <Router>
-                <Switch>
-                    <Route path={'/'} exact={true} component={List} />
-                    <Route path={'/form'} component={FormAdd} />
-                </Switch>
-            </Router>
-        </Layout>
-        <GlobalStyles/>
-    </>
-  );
+        <>
+            <Layout>
+                <Router>
+                    <Switch>
+                        <Route path={'/'} exact={true} component={List} />
+                        <Route path={'/form'} component={FormAdd} />
+                    </Switch>
+                </Router>
+            </Layout>
+            <GlobalStyles/>
+        </>
+    );
 }
 
-const mapStateToProps = state => {
-    return {
-        items: state.items
-    }
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddItem: (value) => dispatch({ type: actionTypes.ADD_ITEM, value}),
-        onDeleteItem: (value) => dispatch({ type: actionTypes.DELETE_ITEM, value })
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
