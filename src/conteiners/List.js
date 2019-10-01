d05fe2e7fad70ec5e9ad898e48363d6c9d5bb2e0
Vladimir Golub <golubvlauri@gmail.com>
2019-10-01 22:57:10 +0200

    complete List container

--- src/conteiners/List.js
+++ src/conteiners/List.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {connect} from "react-redux";
+import styled from 'styled-components';
 
-export default () => {
+import ListElement from '../components/List/ListElement';
+import Controls from '../components/List/Controls';
+
+const StyledControls = styled(Controls)`
+    margin-bottom: 10px;
+`;
+
+const List = ( {items} ) => {
+
     return (
-        <div>List</div>
+        <>
+            <StyledControls />
+            <ListElement
+                items={items}
+            />
+        </>
     )
-}
+};
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.list.items
+    }
+};
+
+export default connect(mapStateToProps)(List);
