27e179a44b65d0e2ab05fe10f35c56b41069da10
Vladimir Golub <golubvlauri@gmail.com>
2019-10-02 10:50:56 +0200

    add new action SORTING_CHANGE

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,4 @@
 export const ADD_ITEM = 'ADD_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
 export const FILTER_LABEL_SELECT = 'FILTER_LABEL_SELECT';
+export const SORTING_CHANGE = 'SORTING_CHANGE';
