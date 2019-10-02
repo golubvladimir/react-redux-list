561d0ca5179ba9a330a97ce491779cb3a664704f
Vladimir Golub <golubvlauri@gmail.com>
2019-10-02 22:08:08 +0200

    add reducer edit

--- src/reducers/list.js
+++ src/reducers/list.js
@@ -33,6 +33,19 @@ export default function list(state = initialState, action) {
     switch (action.type) {
         case actionTypes.ADD_ITEM:
             return state;
+        case actionTypes.EDIT_ITEM:
+            return {
+                ...state,
+                items: state.items.map(item => item.id === action.data.id ?
+                    {
+                        ...item,
+                        name: action.data.name,
+                        email: action.data.email,
+                        label: action.data.label,
+                        dateUpdate: action.data.dateUpdate
+                    } : item
+                )
+            };
         case actionTypes.DELETE_ITEM:
             return {
                 ...state,
