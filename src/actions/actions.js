c5537a7db8da3df04cf752e3cbd04075f7858267
Vladimir Golub <golubvlauri@gmail.com>
2019-10-02 11:16:54 +0200

    add new action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,3 +2,4 @@ export const ADD_ITEM = 'ADD_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
 export const FILTER_LABEL_SELECT = 'FILTER_LABEL_SELECT';
 export const SORTING_CHANGE = 'SORTING_CHANGE';
+export const SEARCH_ITEMS = 'SEARCH_ITEMS';
