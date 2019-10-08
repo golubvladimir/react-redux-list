4bd98254a77114f1212c760a59b085eb619d1a8f
Vladimir Golub <golubvlauri@gmail.com>
2019-10-08 09:22:01 +0200

    add react-persist for storage in localStorage

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducers from './reducers'
+import reducers from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App';
 
-const store = createStore(reducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
